a972e990fe80e1cbea460877c980b429b0cca745
나윤호 (Tura) <soldier4443@gmail.com>
2018-01-05 06:15:27 +0900

    update design

--- js/common_settings.js
+++ js/common_settings.js
@@ -1,1 +1,13 @@
-$('#navigation').load('../navigation.html');
+$('document').ready(function(){
+  $('#navigation').load('navigation.html');
+
+  $(function() {
+    var url = window.location.href;
+
+    $('.nav a').each(function() {
+      if (url == (this.href)) {
+        $(this).closest("li").addClass('active');
+      }
+    })
+  })
+});
